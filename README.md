875ee037f171393ff4b63d59a88784a98011bac2
Nikola Liu <fshk.life@gmail.com>
2020-02-18 23:51:14 +0800

    README updated and show latest result

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# PDF Figure and Table Extraction (Graduate Design) Uncompleted!
+# PDF Figure and Table Extraction (Graduate Design)
 
 ## This project is based on project [CNOCR](https://github.com/breezedeus/cnocr) and [Keras_segmentation](https://github.com/divamgupta/image-segmentation-keras). Thanks for their excellent work!
 
@@ -21,5 +21,22 @@ If you cannot have a good connection, you can download it by yourself in repo's 
 
 ```
 wget -p ./models https://github.com/Alpha-Monocerotis/PDF_FigureTable_Extraction/releases/download/v1.0/resnet_segnet_1.0
+```
+
+## Usage
+Here we have a primary examples to show our result. The original pdf page like this[examples/Material28_images_14.jpg](examples/Material28_images_14.jpg)
+
+Then after extraction, we have:
+[examples/图2.35.jpg](examples/图2.35.jpg)
+
+[examples/图2.36.jpg](examples/图2.36.jpg)
+
+[examples/图2.37.jpg](examples/图2.37.jpg)
+
+Our [log file](examples/log.txt) will generate the following:
+```plain
+图2.35三物质分解的基材料图像。第一行是针对60keV优化的结果，第二行是针对Gd优化的结果。从左到右的每一列分别是PMIMA、Al和Gd的基材料图像。所有图像显示的灰度窗为[-0.5，1.5]。 Material28/图2.35.jpg
+图2.36Gd溶液的重建浓度随光子数的变化。其中显示的是100umaol/ml的Gd的标准差，因为4个的标(准差是基本相同的。 Material28/图2.36.jpg
+图2.37NaCl溶液的重建浓度随光子数的变化。在标准差图像中1.5×10*的结果没有显示，是由于它的标准差太大，超过了40g/100ml。 Material28/图2.37.jpg
 ```
 
--- core.py
+++ core.py
@@ -5,90 +5,88 @@
 # Tool		: Visual Studio Code
 
 import cv2
+import os
 from tqdm import tqdm
-from UnionFind import UnionFind
 from utils import line_segmentation
 from cnocr import CnOcr
-def generate_result():
-    index = 0
-    # TODO(NikolaLiu@icloud.com): Adding function about extract each block and related image
 
 
 
-def extract_text_from_image(path=None):
-    """ CRNN to recognize text"""
-    ocr = CnOcr()
-    reses = ocr.ocr(path)
-    text = ""
-    for res in reses:
-        text += ''.join(res)
-    return text
-
-def get_all_seg_areas(image):
-    assert len(image.shape) == 2 or image.shape[2] == 1, "The input should be a binary image..."
-
-    # directions = [(0, 1), (1, 0), (1, 1)]
-    # DFS Method, StackOverflow !!!
-    # def render_image(x, y):
-    #     # print("dealing with %d %d" % (x, y))
-    #     image[x][y] == 0
-    #     for i, j in directions:
-    #         if x + i < image.shape[0] and y + j < image.shape[1] and image[x+i][y+j] == 255:
-    #             render_image(x+i, y+j)
-
-    # def get_index(x, y):return x*image.shape[1]+y
-    # background_count = 0
-    # # IDEA: Optimized UnionFind Method, avoid stackoverflow by removing recursion. [2020.02.18]
-    # uf = UnionFind(image.shape[0] * image.shape[1])
-    # for i in tqdm(range(image.shape[0])):
-    #     for j in range(image.shape[1]):
-    #         if image[i][j] == 255:
-    #             # for direction in directions:
-    #             if i+1 < image.shape[0]  and image[i+1][j] == 255: #and new_y < col  is true 优化---------------
-    #                 uf.union(get_index(i, j), get_index(i+1, j))
-    #             if  j+1 < image.shape[1] and image[i][j+1] == 255:
-    #                 uf.union(get_index(i, j), get_index(i, j+1))
-    #             if  j+1 < image.shape[1] and i+1 < image.shape[0] and image[i+1][j+1] == 255:
-    #                 uf.union(get_index(i, j), get_index(i+1, j+1))
-    #         else:
-    #             background_count += 1
-
-    nccomps = cv2.connectedComponentsWithStats(image)
-    return nccomps
-
-
-
-def process_area(images, x, y):
-    # TODO(NikolaLiu@icloud.com): Scan column and row respectively, then get the split possibility. You should also consider the situation that hollows in an area could also be the sign of splitting. 
-    pass
-
-
-if __name__ == "__main__":
-    seg_image = cv2.imread('/root/Projects/GraduationDesign/result0/Material28_images_14.jpg')
-    ori_image = cv2.imread('/root/Projects/GraduationDesign/imgs/Material28_images_14.jpg')
-
+def remove_adhesion_area(seg_image, erode_iter=10, kernel=None):
+    """ Remove adhension areas with erode and dialate method, if the adhesion area, the  erode_iter should be set higher to seperate different areas. You can also custom a kernel, which can be more effective.
+    """
     B, G, R = cv2.split(seg_image)
+    # Here we only determine background or block, blocks include tables and figures
     _,RedThresh = cv2.threshold(R,50,255,cv2.THRESH_BINARY)
-    #OpenCV定义的结构矩形元素
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5, 5))
+    
+    if kernel is None:
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(5, 5))
     eroded = cv2.erode(RedThresh,kernel)
-    for i in range(10):
+    for i in range(erode_iter):
         eroded = cv2.erode(eroded,kernel)
     origin = cv2.dilate(eroded,kernel)
-    for i in range(10):
+    for i in range(erode_iter):
         origin = cv2.dilate(origin,kernel)
-    # cv2.imwrite('./x.png', get_all_seg_areas(origin)[1] * 100)
-    blocks = get_all_seg_areas(origin)[2]
-    print(blocks)
-    for index, block in enumerate(blocks[1:]):
+    return origin
+
+
+def generate_result(seg_image, ori_image, erode_iter=10, kernel=None):
+
+    # Forbidden passing image variables, only receive image path as string
+    assert type(seg_image) == str and type(ori_image) == str, "seg_image and ori_image should be strings of image path."
+    file_name = seg_image.split('/')[-1].split('_')[0]
+    seg_image = cv2.imread(seg_image)
+    ori_image = cv2.imread(ori_image)
+    assert len(seg_image.shape) == 3 and seg_image.shape[2] == 3, "Channel number of Segmentation result should be 3"
+
+    def save(title=None, block=None, file_name=None, tag=None):
+        """save detected area and save log"""
+
+        # Check required folders and files
+        if not os.path.isdir("./results"):
+            os.system("mkdir results")
+        if not os.path.isdir("./results/" + file_name):
+            os.system("mkdir results/" + file_name)
+        if not os.path.isfile("./log.txt"):
+            os.system("touch log.txt")
+
+        # Tag should be determined, otherwise the model may not work properly.
+        assert tag is not None, "Tag cannot be None, please check your data or issue on GitHub."
+        save_name = "unkonwn"
+        if title is None:
+            title = "This " + tag + " cannot find subscript."
+        else:
+            for i in range(title.__len__()):
+                # TODO(NikolaLiu@icloud.com): English version should be considered, as in English-version papers, the first work should be Figure or Table.
+                if title[i] not in ('图', '表', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'):
+                    # Normally we won't meet 图1.100
+                    if i > 5:
+                        i = 5
+                    save_name = title[:i]
+                    break
         
+        cv2.imwrite("./results/" + file_name + '/' + save_name  + ".jpg", ori_image[block[1]:block[1]+block[3], block[0]:block[0]+block[2]])
+        with open("log.txt", 'a') as f:
+            f.writelines(title + " " + file_name + '/' + save_name + ".jpg\n")
+    
+    # Remove adhesion area using morphology methods
+    seg_image_binary = remove_adhesion_area(seg_image, erode_iter, kernel)
+
+    # Getting all blocks in binary segmentation image
+    blocks = get_all_seg_areas(seg_image_binary)[2]
+
+    # The first block is the background, so start from 1
+    for index, block in enumerate(blocks[1:]):
         if block[2] >= seg_image.shape[0] // 2:
             print("Single Page mode.")
         else:
             print("Cannot define Page mode, default mode is single page mode.")
+
         # Here the coordinate is managed (y, x)
         left_up = (block[1], block[0])
         right_down = (block[1]+block[3], block[0]+block[2])
+
+        # Use the center of area to determine the category
         center_point = (block[1]+block[3]//2, block[0]+block[2]//2)
         tag = ""
         if abs(seg_image[center_point[0]][center_point[1]][0] - 156) <= 3:
@@ -99,17 +97,47 @@ if __name__ == "__main__":
             tag = "figure"
             print("Figure found!")
             scan_area = ori_image[block[1]+block[3]:]
-        
+
+        # Get all possible areas that include title
         line_segs = line_segmentation(scan_area,flag=1 ,tag=tag)
+
+        # If the category is table, the return sequence of areas should be reversed
         if tag == 'table':
             line_segs = line_segs.reverse()
-        # print(line_segs)
-        # seg_line_areas = []
+
+        # Extract text by lines, this could improve the accuracy, proved by test.
+        line_seg_areas = []
+        for line_seg in line_segs:
+            line_seg_areas.append(scan_area[line_seg[0]:line_seg[1]])
+        title = extract_text_from_image(line_seg_areas)
+        # Extract as a whole when using ocr.ocr()
+        # title = extract_text_from_image(scan_area[line_segs[0][0]:line_segs[-1][1]])
+        save(title, block, file_name, tag)
         
-        # for line_seg in line_segs:
-        #     seg_line_areas.append(scan_area[line_seg[0]:line_seg[1]])
-        title = extract_text_from_image(scan_area[line_segs[0][0]:line_segs[-1][1]])
-        print(title)
-        cv2.imwrite('title' + '.png', ori_image[block[1]:block[1]+block[3], block[0]:block[0]+block[2]])
+
+
+
+def extract_text_from_image(path=None):
+    """ CRNN to recognize text"""
+    ocr = CnOcr()
+    reses = ocr.ocr_for_single_lines(path)
+    text = ""
+    for res in reses:
+        text += "".join(res)
+    return text
+
+def get_all_seg_areas(image):
+    """ Get all areas from binary segmentation result. """
+    assert len(image.shape) == 2 or image.shape[2] == 1, "The input should be a binary image..."
+    nccomps = cv2.connectedComponentsWithStats(image)
+    return nccomps
+
+
+if __name__ == "__main__":
+    seg_image = '/root/Projects/GraduationDesign/result0/Material28_images_66.jpg'
+    ori_image = '/root/Projects/GraduationDesign/imgs/Material28_images_66.jpg'
+    generate_result(seg_image, ori_image)
+
+    
 
 
